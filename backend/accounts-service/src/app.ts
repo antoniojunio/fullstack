9e6c9588c4f8834f99c2368c9dfa546595baffaa
antoniojunio <juniordsj@gmail.com>
2023-08-10 21:02:04 -0300

    feat: add config routers accounts

--- backend/accounts-service/src/app.ts
+++ backend/accounts-service/src/app.ts
@@ -1,10 +1,13 @@
 //app.
 import express from 'express'
 import helmet from 'helmet'
+import accountsRouter from './routes/accounts'
 
 const app = express()
 app.use(helmet())
 app.use(express.json())
+
+app.use(accountsRouter)
 
 const port =parseInt(`${process.env.PORT}`)
 
